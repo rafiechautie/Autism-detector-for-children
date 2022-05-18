9fc57ddf7d31dfd1f0638c42692d4198b964f60b
raflisalam <raflisalam.dokumen@gmail.com>
2022-05-18 17:57:10 +0800

    Proses Auth

---
+++ Mobile-Development/AutismDetection/app/src/main/java/com/capstoneproject/autismdetection/ui/auth/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.capstoneproject.autismdetection.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstoneproject.autismdetection.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
